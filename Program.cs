5308577998a72bf24202acf5a845688083be7cac
Grimchic <vladis.nr@mail.ru>
2026-09-17 10:57:03 +0300

    test

---
+++ Program.cs
@@ -0,0 +1,3 @@
+﻿System.Console.WriteLine($"Компьютер:{Environment.MachineName}");
+System.Console.WriteLine($"Пользователь:{Environment.UserName}");
+System.Console.WriteLine($"Дата и время:{DateTime.Now:dd.MM.yyyy HH:mm}");
